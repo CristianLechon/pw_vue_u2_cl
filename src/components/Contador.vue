312d5adc6afd9e8f3ff669ef5625652de196e6c2
CristianLechon <calechon@uce.edu.ec>
2025-11-26 19:45:23 -0500

    Taller 14

--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,16 +1,79 @@
 <template>
+  <div>
+    <h1>{{ tit }}</h1>
+    <p>{{ numero }} <sup>2</sup>={{ calcularCuadrados }}</p>
+    <p>{{ numero }} <sup>2</sup>={{ calcularCuadrados }}</p>
+    <p>{{ numero }} <sup>2</sup>={{ calcularCuadrados }}</p>
+    <p>{{ numero }} <sup>3</sup>={{ calcularCubo }}</p>
+    <p>{{ numero }} <sup>3</sup>={{ obtenerCubo() }}</p>
     <div>
-        <h1>Universidad Central del Ecuador</h1>
-        <h2>Carrera de Computación</h2>
+      <!-- directiva empieza con v- -->
+      <button @:click="incrementar()">+1</button>
+      <button v-on:click="decrementar()">-1</button>
+      <button v-if="mostrar">prueba</button>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
+  // Propiedades reactivas en vue
+  data() {
+    //opcion mas de openapi
+    return {
+      numero: this.inicio,
+      titulo: "Contador",
+    };
+  },
+  // No importa el orden
+  // Metodos
+  methods: {
+    obtenerCuadrados() {
+      console.log("Entro a obtener cuadrado");
+      return this.numero * this.numero;
+    },
+    obtenerCubo() {
+      return this.numero * this.numero * this.numero;
+    },
+    incrementar() {
+      console.log(this.tit);
+      this.numero++;
+    },
+    decrementar() {
+      this.numero--;
+    },
+  },
+  // Propiedades computadas
+  // Solo se ejecutan cuando solo si cambia el resultado del cuerpo, caso contrario no se ejecuta la propiedad lo guarda en cache
+  computed: {
+    calcularCuadrados() {
+      console.log("Entro a propiedad computada");
+      return this.numero * this.numero;
+    },
+    calcularCubo() {
+      return this.numero * this.numero * this.numero;
+    },
+  },
 
-}
+  // Props
+  // Son variables que yo puedo instanciar o dar un valor, extermo de mi componente.
+  // Variables a nivel de componente, son seteados por el padre
+  //props: ["tit", "inicio", "mostrar"], // declaracion basica de props.
+  props: {
+    tit: String,
+    // con mas detalle
+    inicio: {
+      type: Number,
+      default: 99,
+      validator(value) {
+        return value > 10;
+      },
+    },
+    mostrar: Boolean,
+  },
+  /* String, Numbre, Boolean, Array, Object, Date, Function, BigInt */
+};
 </script>
 
 <style>
-
 </style>
--- src/views/ContadorView.vue
+++ src/views/ContadorView.vue
@@ -1,19 +1,22 @@
 <template>
-    <div>
-        <Contador/>
-        <Contador/>
-    </div>
+  <div>
+    <!-- Cada componete tiene su estado -->
+    <!-- <Contador tit="Contador 1" v-bind:inicio="4+4"/> -->
+    <Contador tit="Contador 1" v-bind:inicio="20" :mostrar="true"/>
+    <Contador tit="Contador 2" :inicio="4" :mostrar="false"/>
+    <!-- <Contador /> -->
+    <!-- <Contador /> -->
+  </div>
 </template>
 
 <script>
-import Contador from '../components/Contador.vue';
+import Contador from "../components/Contador.vue";
 export default {
-    components: {
-        Contador,
-    },
-}
+  components: {
+    Contador,
+  },
+};
 </script>
 
 <style>
-
 </style>
